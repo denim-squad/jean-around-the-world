231bf1a87e99d9619ebdcd3a0506af66b6f1c494
Hai Lin Zhang <hzhan339@hotmail.com>
2019-06-04 22:29:07 -0700

    Background image

--- imports/ui/pages/preferences/preferences.page.jsx
+++ imports/ui/pages/preferences/preferences.page.jsx
@@ -2,11 +2,33 @@ import React from 'react';
 import Navbar from '../../shared_components/navbar/navbar';
 import './preferences.page.css';
 
+const images = [
+    "/grand_canyon.svg",
+    "/lighthouse.svg",
+    "/japan.svg",
+    "/greece.svg",
+    "/eiffel_tower.svg",
+    "/london.svg",
+    "/ski.svg",
+    "/venice.svg",
+    "/china.svg",
+    "/new_york.svg",
+];
+
 class PreferencesPage extends React.Component {
+  
+    constructor() {
+        super();
+        this.state = { 
+            imgPath: "url(" + images[~~(Math.random() *10)] + ")" 
+        };
+    }
+  
     render() {
         return <div className="preferences-page-container">
             <Navbar />
-            <div />
+            <div className="preferences-background" style={{ backgroundImage: this.state.imgPath }}>
+            </div>
         </div>
     }
 }
