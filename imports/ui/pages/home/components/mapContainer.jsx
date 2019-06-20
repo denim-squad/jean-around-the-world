ae6d341ddcea7039810b18a86f574f051e387cbf
jessicawu <jessicawu.98@hotmail.ca>
2019-06-20 15:12:42 -0700

    patch up typo error in pull

--- imports/ui/pages/home/components/mapContainer.jsx
+++ imports/ui/pages/home/components/mapContainer.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 const mapStyles = {
   width: '100%',
-  height: '100%'l
+  height: '100%'
 };
 
 export class MapContainer extends React.Component {
@@ -20,7 +20,7 @@ export class MapContainer extends React.Component {
       const { latLng } = coord;
       this.setState({
         activeMarker: {
-          lat: latLng.lat(), 
+          lat: latLng.lat(),
           lng: latLng.lng()
         }
       });
@@ -43,7 +43,7 @@ export class MapContainer extends React.Component {
         onClick={this.setMarkerLocation}>
           <Marker
             position={{ lat: markerCoords.lat, lng: markerCoords.lng }}
-          /> 
+          />
           <Circle
             radius={this.props.radius}
             center={markerCoords}
@@ -52,18 +52,18 @@ export class MapContainer extends React.Component {
             fillColor='#FFB26B'
             fillOpacity={0.4}
           />
-    </Map>   
+    </Map>
     }
 }
 
 const mapStateToProps = (state) => {
   console.log(state.map.initialCenter);
-	return { 
+	return {
       radius: state.map.radius,
       initialCenter: state.map.initialCenter
   };
 }
- 
+
 export default connect(mapStateToProps)(GoogleApiWrapper({
   apiKey: ('') // paste API key here each time, DO NOT COMMIT.
 })(MapContainer));
