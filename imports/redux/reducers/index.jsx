292198e33a5c0055aaeb62fe467af8703b4fed0a
John Sastrillo <john.sastrillo@gmail.com>
2019-06-20 20:42:06 -0700

    add stub reducers for preferences

--- imports/redux/reducers/index.jsx
+++ imports/redux/reducers/index.jsx
@@ -1,5 +1,29 @@
 import { combineReducers } from 'redux';
-import { SHOW_MODAL, HIDE_MODAL, SET_RADIUS, SET_CENTER , LOGIN_USER, LOGOUT_USER} from '../actions/index';
+import { SHOW_MODAL, HIDE_MODAL, SET_RADIUS, SET_CENTER , LOGIN_USER, LOGOUT_USER,
+  GET_PREFERENCES, ADD_BLACKLIST, ADD_FAVOURITES, REMOVE_BLACKLIST, REMOVE_FAVOURITES } from '../actions/index';
+
+let userInfos = [
+  { email: "john.sastrillo@gmail.com", firstName: "John", lastName: "Sastrillo",
+    preferences: {
+      blacklist: [],
+      favourites: []
+    }},
+  { email: "hailin.zhang@gmail.com", firstName: "Hailin", lastName: "Zhang",
+    preferences: {
+      blacklist: [],
+      favourites: []
+    }},
+  { email: "jessica.wu@gmail.com", firstName: "Jessica", lastName: "Wu",
+    preferences: {
+      blacklist: [],
+      favourites: []
+    }},
+  { email: "wesley.ferguson@gmail.com", firstName: "Wesley", lastName: "Ferguson",
+    preferences: {
+      blacklist: [],
+      favourites: []
+    }},
+]
 
 const initialState = {
   isModalShown: false,
@@ -8,10 +32,7 @@ const initialState = {
     lat: 49.263749,
     lng: -123.247480
   },
-  username: 0,
-  preferences: [],
-  blacklist: [],
-  userInfo: {}
+  isSignedIn: false,
 }
 
 function loginReducer(state = initialState, action) {
@@ -21,9 +42,32 @@ function loginReducer(state = initialState, action) {
     case HIDE_MODAL:
       return { ...state, isModalShown: false };
     case LOGIN_USER:
-      return { ...state, isSignedIn: true, username: action.username};
+      for(let userInfo of userInfos){
+        if(userInfo.email === action.email){
+          console.log("email in info");
+          return { ...state, isSignedIn: true, username: `${userInfo.firstName} ${userInfo.lastName}`}
+          break;
+        }
+      }
     case LOGOUT_USER:
       return { ...state, isSignedIn: false, username: ""};
+    default:
+      return state;
+  }
+}
+
+function preferencesReducer(state = initialState, action){
+  switch (action.type) {
+    case GET_PREFERENCES:
+      return []; //stub
+    case ADD_BLACKLIST:
+      return []; //stub
+    case ADD_FAVOURITES:
+      return []; //stub
+    case REMOVE_BLACKLIST:
+      return []; //stub
+    case REMOVE_FAVOURITES:
+      return []; //stub
     default:
       return state;
   }
@@ -43,4 +87,5 @@ function mapReducer(state = initialState, action) {
 export default combineReducers({
   login: loginReducer,
   map: mapReducer,
+  preferences: preferencesReducer
 });
