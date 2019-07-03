c4ba55c71624c1f79d8333b3cb50c88a801f9fa0
Wes Ferguson <pwesferguson@gmail.com>
2019-07-03 06:38:08 -0700

    add test packages and stubs

--- imports/api/places.test.js
+++ imports/api/places.test.js
@@ -1,2 +1,23 @@
 import getNearbyPlaces from './places';
+import chai from 'chai';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+
+describe('getPlaces', () => {
+  
+  it('returns results in a promise', function () {
+    //todo
+  });
+  
+  it('only returns results with the specified type', function () {
+    //todo 
+  });
+
+  it('returns sum of quantities number of places', function () {
+    //todo
+  });
+}) 
