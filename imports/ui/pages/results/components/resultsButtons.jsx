20985fa3cf03c81f49660e88089aa810cd1b41d2
jessicawu <jessicawu.98@hotmail.ca>
2019-07-18 00:10:45 -0700

    add results text

--- imports/ui/pages/results/components/resultsButtons.jsx
+++ imports/ui/pages/results/components/resultsButtons.jsx
@@ -4,40 +4,43 @@ import { BootstrapButton } from '../../../shared_components/MUI/button/bootstrap
 
 class ResultsButtons extends React.Component {
     render() {
-        return <div className="results-buttons-container">
-           <div>
-              {/* spacing  */}
-           </div>
-           <BootstrapButton
-               className="save-trip-button"
-               variant="contained"
-               size="small"
-               color="primary">
-               SAVE TRIP
-           </BootstrapButton>
-           <div>
-              {/* spacing  */}
-           </div>
-           <BootstrapButton
-               className="add-calendar-button"
-               variant="contained"
-               size="small"
-               color="primary">
-               ADD TO CALENDAR
-           </BootstrapButton>
-           <div>
-              {/* spacing  */}
-           </div>
-           <BootstrapButton
-               className="new-trip-button"
-               variant="contained"
-               size="small"
-               color="primary">
-               NEW TRIP
-           </BootstrapButton>
-           <div>
-              {/* spacing  */}
-           </div>
+        return <div className="results-container">
+            WE FOUND JUST THE TRIP FOR YOU!
+            <div className="results-buttons-container">
+               <div>
+                  {/* spacing  */}
+               </div>
+               <BootstrapButton
+                   className="save-trip-button"
+                   variant="contained"
+                   size="small"
+                   color="primary">
+                   SAVE TRIP
+               </BootstrapButton>
+               <div>
+                  {/* spacing  */}
+               </div>
+               <BootstrapButton
+                   className="add-calendar-button"
+                   variant="contained"
+                   size="small"
+                   color="primary">
+                   ADD TO CALENDAR
+               </BootstrapButton>
+               <div>
+                  {/* spacing  */}
+               </div>
+               <BootstrapButton
+                   className="new-trip-button"
+                   variant="contained"
+                   size="small"
+                   color="primary">
+                   NEW TRIP
+               </BootstrapButton>
+               <div>
+                  {/* spacing  */}
+               </div>
+            </div>
         </div>;
     }
 }
