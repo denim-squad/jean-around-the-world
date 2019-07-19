0e52254a5b4377d138a2df7207c5fca325b89cb7
wesley.ferguson <wesley.ferguson@trulioo.com>
2019-07-19 15:09:02 -0700

    go for privacy by obscurity

--- imports/api/key.js
+++ imports/api/key.js
@@ -1,4 +1,4 @@
 import { Meteor } from 'meteor/meteor'
-export default API_KEY = process.env.API_KEY;
+export default API_KEY = Meteor.settings.public.API_KEY;
 console.log("process.env: ", process.env);
 console.log("meteor.settings:", Meteor.settings);
