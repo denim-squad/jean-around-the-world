fdc8617921d005ce8de236a5059c3b6caf2ddbf3
Wes Ferguson <pwesferguson@gmail.com>
2019-07-25 19:42:01 -0700

    try meteor.methods

--- imports/api/places/methods.js
+++ imports/api/places/methods.js
@@ -1,5 +1,6 @@
 import SimpleSchema from 'simpl-schema';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { Meteor } from 'meteor/meteor';
 
 export const FETCH_PLACES_NAME = 'places.fetchPlaces';
 
@@ -10,15 +11,8 @@ const fetchPlacesSchema = new SimpleSchema({
   type: String
 });
 
-export const fetchPlaces = new ValidatedMethod({
-  name: FETCH_PLACES_NAME,
-  validate: null,
-  run({
-    initialCenter,
-    radius,
-    budgetRange,
-    type
-  }) {
+Meteor.methods({
+  fetchPlaces(initialCenter, radius, budgetRange, type) {
     if (this.isSimulation) {
       // TODO Simulation code for the client (optional)
       console.log("In client method call, parameters:", initialCenter, radius, budgetRange, type);
@@ -30,4 +24,26 @@ export const fetchPlaces = new ValidatedMethod({
       return resultsAsPromise;
     }
   }
-});
+})
+
+// export const fetchPlaces = new ValidatedMethod({
+//   name: FETCH_PLACES_NAME,
+//   validate: null,
+//   run({
+//     initialCenter,
+//     radius,
+//     budgetRange,
+//     type
+//   }) {
+//     if (this.isSimulation) {
+//       // TODO Simulation code for the client (optional)
+//       console.log("In client method call, parameters:", initialCenter, radius, budgetRange, type);
+//     } else if (this.isServer) {
+//       import { fetchPlacesFromServer } from './server/fetchPlaces';
+//       console.log("in fetchPlaces method");
+//       const resultsAsPromise = fetchPlacesFromServer(initialCenter, radius, budgetRange, type);
+//       console.log("resultsAsPromise:", resultsAsPromise);
+//       return resultsAsPromise;
+//     }
+//   }
+// });
