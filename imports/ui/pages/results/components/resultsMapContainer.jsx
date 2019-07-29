a47f7bf9a158abbadf8def1c36d667a5fa5d3155
Hai Lin Zhang <hzhan339@hotmail.com>
2019-07-28 22:08:03 -0700

    in-progress map results

--- imports/ui/pages/results/components/resultsMapContainer.jsx
+++ imports/ui/pages/results/components/resultsMapContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map, GoogleApiWrapper, Marker, Circle } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, Polyline } from 'google-maps-react';
 import { connect } from 'react-redux';
 
 const mapStyles = {
@@ -14,15 +14,25 @@ export class ResultsMapContainer extends React.Component {
     }
 
     render() {
+      const triangleCoords = [
+        {lat: 25.774, lng: -80.190},
+        {lat: 18.466, lng: -66.118},
+        {lat: 32.321, lng: -64.757},
+        {lat: 25.774, lng: -80.190}
+      ];
       return <Map
           google = {this.props.google}
           zoom = {14}
           style = {mapStyles}
-          initialCenter = {{
-            lat: 49.263749,
-            lng: -123.247480
-          }}
-          >
+          initialCenter = {this.props.initialCenter}>
+          {triangleCoords.map((coord) => {
+            return <Marker position={coord}/>
+          })}
+          <Polyline
+          path={triangleCoords}
+          strokeColor="#0000FF"
+          strokeOpacity={0.8}
+          strokeWeight={2} />
         </Map>
     }
 }
@@ -30,6 +40,7 @@ export class ResultsMapContainer extends React.Component {
 const mapStateToProps = (state) => {
 	return {
       //TODO: return paths
+      initialCenter: state.map.initialCenter,
   };
 }
 
