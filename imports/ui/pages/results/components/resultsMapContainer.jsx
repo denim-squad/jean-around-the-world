a7e07b64bb042ba2991a7599ba1dca4290863c30
Hai Lin Zhang <hzhan339@hotmail.com>
2019-07-31 22:20:25 -0700

    mock for results

--- imports/ui/pages/results/components/resultsMapContainer.jsx
+++ imports/ui/pages/results/components/resultsMapContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map, GoogleApiWrapper, Marker, Polyline } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, Polyline, InfoWindow } from 'google-maps-react';
 import { connect } from 'react-redux';
 
 const mapStyles = {
@@ -11,6 +11,26 @@ export class ResultsMapContainer extends React.Component {
 
     constructor() {
       super();
+      this.state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+      };
+    }
+
+    setActiveMarker = (props, marker, e) => {
+      this.setState({
+        activeMarker: marker,
+        showingInfoWindow: true
+      });
+    }
+
+    closeActiveMaker = (props) => {
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        })
+      }
     }
 
     render() {
@@ -24,16 +44,24 @@ export class ResultsMapContainer extends React.Component {
           google = {this.props.google}
           zoom = {14}
           style = {mapStyles}
-          initialCenter = {this.props.initialCenter}>
+          initialCenter = {this.props.initialCenter}
+          onClick={this.closeActiveMaker}>
           {triangleCoords.map((coord) => {
-            return <Marker position={coord}/>
+            return <Marker position={coord} onClick={this.setActiveMarker} name={'Current location'}/>
           })}
           <Polyline
-          path={triangleCoords}
-          strokeColor="#0000FF"
-          strokeOpacity={0.8}
-          strokeWeight={2} />
-        </Map>
+            path={triangleCoords}
+            strokeColor="#0000FF"
+            strokeOpacity={0.8}
+            strokeWeight={2} />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>ya YEET</h1>
+            </div>
+        </InfoWindow>
+      </Map>
     }
 }
 
