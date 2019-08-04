296f01912c16a3d7391b67c2e75cbe254dac84e5
pwf <pwf@students.cs.ubc.ca>
2019-08-03 19:09:30 -0700

    try moving method out of server to see client logs

--- imports/api/places/getPlaceDetails.js
+++ imports/api/places/getPlaceDetails.js
@@ -3,7 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { HTTP } from 'meteor/http';
 import SimpleSchema from 'simpl-schema';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
-import { GET_PLACE_DETAILS_NAME } from '../methods';
+import { GET_PLACE_DETAILS_NAME } from './methods';
 
 const getPlaceDetailsSchema = new SimpleSchema({
   id: String,
@@ -16,12 +16,15 @@ export default new ValidatedMethod({
   run(id, fields) {
     if (!this.isSimulation) {
       const url = 'https://maps.googleapis.com/maps/api/place/details/output';
+      const API_KEY = Meteor.settings.API_KEY || 'could not find private key';
       try {
-        const result = HTTP.call('GET', url, { id, fields });
+        const result = HTTP.call('GET', url, { API_KEY, id, fields });
         return result;
       } catch (error) {
         throw new Meteor.Error(error);
       }
+    } else {
+      console.log('id:', id, 'fields:', fields);
     }
   },
 });
