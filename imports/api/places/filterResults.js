59a7a2d5636217a6107a918b5d888d6589bc79df
John Sastrillo <john.sastrillo@gmail.com>
2019-08-05 14:52:11 -0700

    fix bug

--- imports/api/places/filterResults.js
+++ imports/api/places/filterResults.js
@@ -1,13 +1,15 @@
 import levenshtein from 'js-levenshtein';
 
 const isNotBlacklisted = (result, blacklist) => {
-  blacklist.every((blacklistItem) => {
+  return blacklist.every((blacklistItem) => {
     const { name } = result;
     const lowerCaseBlacklist = blacklistItem.toLowerCase();
     const lowerCaseResultName = name.toLowerCase();
 
+    console.log(levenshtein(lowerCaseBlacklist, lowerCaseResultName));
+
     return !lowerCaseBlacklist.includes(lowerCaseResultName)
-      || levenshtein(lowerCaseBlacklist, lowerCaseResultName) > 3;
+      || levenshtein(lowerCaseBlacklist, lowerCaseResultName) < 3;
   });
 };
 
@@ -15,10 +17,11 @@ const isInBudgetRange = (result, budgetRange) => {
   return budgetRange[0] <= result.price_level && result.price_level <= budgetRange[1];
 };
 
-export default (results, budgetRange, minimumRating, blacklist) => (
-  results.filter(result => (
+export default (results, budgetRange, minimumRating, blacklist) => {
+  console.log(results);
+  return results.filter(result => (
     minimumRating <= result.rating
     && isInBudgetRange(result, budgetRange)
     && isNotBlacklisted(result, blacklist)
-  ))
-);
+  ));
+};
