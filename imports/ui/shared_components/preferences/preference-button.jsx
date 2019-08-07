beac12216ccf240cd1fddf307c74820985498a26
John Sastrillo <32103379+js1998@users.noreply.github.com>
2019-08-07 15:58:14 -0700

    Save Travel Functionality
    * save work
    
    * save travels
    
    * save previous travel and delete under stub name
    
    * allow saving travel name
    
    * take travel and go to reducer
    
    * fix result page
    
    * clean-up functions
    
    * dont allow user not logged in to save
    
    * fixed both modals showing up

--- imports/ui/shared_components/preferences/preference-button.jsx
+++ imports/ui/shared_components/preferences/preference-button.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import Chip from '@material-ui/core/Chip';
+import { createBrowserHistory } from 'history';
 import { connect } from 'react-redux';
 import {
   removeBlacklist,
   removeFavourites,
   deletePrevTravel,
+  getPrevTravel,
   REMOVE_BLACKLIST,
   REMOVE_FAVOURITES,
   DELETE_PREVIOUS_TRAVEL,
 } from '../../../redux/actions/index';
 import './preference-button.css';
+import LoadingSpinner from '../loading/loadingSpinner';
+
+const history = createBrowserHistory({ forceRefresh: true });
 
 class PreferenceButton extends React.Component {
+  constructor() {
+    super();
+    this.loadingSpinner = React.createRef();
+  }
+
       handleDelete = (type, name) => {
         switch (type) {
           case REMOVE_BLACKLIST:
@@ -21,24 +31,46 @@ class PreferenceButton extends React.Component {
             this.props.removeFavourites(name);
             break;
           case DELETE_PREVIOUS_TRAVEL:
-            this.props.deletePrevTravel(name);
+            this.props.deletePrevTravel(name, this.props.userId);
             break;
           default:
         }
       }
 
+      handleClick = async (isTravel, travelName) => {
+        if (isTravel) {
+          this.props.getPrevTravel(travelName, this.props.userId);
+          this.loadingSpinner.current.style.display = 'block';
+          await setTimeout(() => {
+            this.loadingSpinner.current.style.display = 'none';
+            history.push('/results');
+          }, 5000);
+        }
+      }
+
+
       render() {
         return (
-          <Chip
-            label={this.props.name}
-            onDelete={() => { this.handleDelete(this.props.type, this.props.name); }}
-            className="preference-chip"
-          />
+          <div>
+            <LoadingSpinner ref={this.loadingSpinner} />
+            <Chip
+              label={this.props.name}
+              onClick={() => { this.handleClick(this.props.isTravel, this.props.name); }}
+              onDelete={() => { this.handleDelete(this.props.type, this.props.name); }}
+              className="preference-chip"
+            />
+          </div>
         );
       }
 }
 
 const mapStateToProps = state => ({
+  userId: state.user.userId,
 });
 
-export default connect(mapStateToProps, { removeBlacklist, removeFavourites, deletePrevTravel })(PreferenceButton);
+export default connect(mapStateToProps, {
+  removeBlacklist,
+  removeFavourites,
+  deletePrevTravel,
+  getPrevTravel,
+})(PreferenceButton);
