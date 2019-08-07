94695fd7c83848ed4c9716381324634a40bca38a
pwf <pwf@students.cs.ubc.ca>
2019-08-06 19:47:57 -0700

    fix errors

--- imports/api/places/filterResults.js
+++ imports/api/places/filterResults.js
@@ -1,25 +1,20 @@
 import levenshtein from 'js-levenshtein';
 
-const isNotBlacklisted = (result, blacklist) => {
-  return blacklist.every((blacklistItem) => {
-    const { name } = result;
-    const lowerCaseBlacklist = blacklistItem.toLowerCase();
-    const lowerCaseResultName = name.toLowerCase();\
-    
-    return !lowerCaseBlacklist.includes(lowerCaseResultName)
+const isNotBlacklisted = (result, blacklist) => blacklist.every((blacklistItem) => {
+  const { name } = result;
+  const lowerCaseBlacklist = blacklistItem.toLowerCase();
+  const lowerCaseResultName = name.toLowerCase();
+
+  return !lowerCaseBlacklist.includes(lowerCaseResultName)
       || levenshtein(lowerCaseBlacklist, lowerCaseResultName) < 3;
-  });
-};
+});
 
-const isInBudgetRange = (result, budgetRange) => {
-  return budgetRange[0] <= result.price_level && result.price_level <= budgetRange[1];
-};
+const isInBudgetRange = (result, budgetRange) => (
+  budgetRange[0] <= result.price_level && result.price_level <= budgetRange[1]
+);
 
-export default (results, budgetRange, minimumRating, blacklist) => {
-  console.log(results);
-  return results.filter(result => (
-    minimumRating <= result.rating
+export default (results, budgetRange, minimumRating, blacklist) => results.filter(result => (
+  minimumRating <= result.rating
     && isInBudgetRange(result, budgetRange)
     && isNotBlacklisted(result, blacklist)
-  ));
-};
+));
