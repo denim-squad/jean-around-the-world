c316a4e3f96bbf41e2b0171cbb7b3ad642055341
John Sastrillo <john.sastrillo@gmail.com>
2019-08-08 00:41:37 -0700

    add background to profile and add blacklist on enter

--- imports/ui/pages/preferences/components/stepBox.jsx
+++ imports/ui/pages/preferences/components/stepBox.jsx
@@ -76,6 +76,12 @@ class StepBox extends React.Component {
                 placeholder="McDonald's"
                 margin="none"
                 inputRef={(input) => { this.blacklistInput = input; }}
+                onKeyPress={(event) => {
+                  if (event.key === 'Enter') {
+                    event.preventDefault();
+                    this.handleAddBlacklist();
+                  }
+                }}
               />
               <BootstrapButton
                 className="add-button"
